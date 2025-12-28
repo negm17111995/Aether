1401ceda8383bb86d1a33ad537b1d6f9d3d293ab
Negm <ahmednegm.1711@.com>
2025-12-28 15:37:25 +0300

    🧹 Clean repo: Remove all non-Aether files for 100% pure release

--- tests/test_asm.s
+++
@@ -1,5 +0,0 @@
-.global _main
-.align 4
-_main:
-    mov x0, #30      // return 30
-    ret              // let libc handle exit
--- tests/tiny.s
+++
@@ -1,6 +0,0 @@
-.global _main
-.align 4
-_main:
-    mov x0, #30
-    mov x16, #1
-    svc #0x80
